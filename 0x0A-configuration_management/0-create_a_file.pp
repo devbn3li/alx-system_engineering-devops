54db6b89d5fea956972c99cca350cecdf7ff9729
devbn3li <fudwhc@gmail.com>
2023-11-24 21:50:25 +0200

    pushd 0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-#create a file in /tmp
+# create a file in /tmp
 file {  '/tmp/school':
-    mode => 0744,
-    owner => www-data,
-    group => www-data,
+    ensure => 'file',
+    mode => '0744',
+    owner => 'www-data',
+    group => 'www-data',
     content => 'I love Puppet',
 }
