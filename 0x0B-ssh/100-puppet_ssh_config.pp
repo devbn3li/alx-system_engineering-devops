a53e2ebdaf31d8ba3580e64e0697fb08f3876b3e
devbn3li <fudwhc@gmail.com>
2023-11-27 04:23:28 +0200

    p

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,16 @@
-#!/user/bin/env bash
-# make changes to our configuration file
+#Create a File in /tmp
+file { '/etc/ssh/ssh_config':
+  ensure  => file,
+  content => "# This is the ssh client system-wide configuration file.
 
-file { 'ect/ssh/ssh_config':
-        ensure => present,
+Include /etc/ssh/ssh_config.d/*.conf
 
-content =>"
-
-          #SSH client configuration
-          host *
-          IdentityFile ~/.ssh/school
-          passwordAuthentication no
+Host *
+    PasswordAuthentication no
+    IdentityFile ~/.ssh/school
+    SendEnv LANG LC_*
+    HashKnownHosts yes
+    GSSAPIAuthentication yes
+",
+  mode    => '0644',
 }
