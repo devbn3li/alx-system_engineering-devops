e9d5acecffff0e4a3f14fb187323b7c3852d444f
devbn3li <fudwhc@gmail.com>
2024-01-02 23:20:25 +0200

    poooooo

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    todo = requests.get(url + "todos").json()
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({user.get("id"): [{
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": user.get("username")
+        } for task in todo if user.get("id") == task.get("userId")]
+        for user in users}, jsonfile)
