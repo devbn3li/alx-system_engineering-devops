ad6a7ef5d261f2d58c157af95a346ad135ff24eb
devbn3li <fudwhc@gmail.com>
2024-01-17 04:02:13 +0200

    up

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Function to query the Reddit API and return the number of subscribers
-for a given subreddit."""
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Return the top 10 hot posts for a given subreddit."""
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    response = requests.get(url, headers={'User-Agent': 'app/1.0'})
-    data = response.json()
-
-    if response.status_code == 200:
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project: v1.0.0"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
