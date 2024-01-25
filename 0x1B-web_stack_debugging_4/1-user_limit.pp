4fc584cddcecb0e69b4b20c5c98d3f47d1fea425
devbn3li <fudwhc@gmail.com>
2024-01-25 22:53:45 +0200

    upload

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Increase hard file limit for Holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+# Increase soft file limit for Holberton user
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
